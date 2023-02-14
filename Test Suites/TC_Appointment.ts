9b28208f478288491e4081046b3eb906295615cb
yudhprtm <yudhprtm@gmail.com>
2023-02-14 19:12:46 +0700

    init

---
+++ Test Suites/TC_Appointment.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_Appointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>dad2a59d-3d16-4dcb-aa85-6b73cdff1ff9</testSuiteGuid>
+   <testCaseLink>
+      <guid>64707fde-5253-4cf5-9632-642cc65ce319</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_2.1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6ac2e3ab-b0ab-4157-a776-e8e39d161610</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_2.2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TC_Login.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f24d549d-c6fc-476b-81db-b417f0bf1fe2</testSuiteGuid>
+   <testCaseLink>
+      <guid>8173b929-0efc-4c0b-b5af-59d248404d85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_1.1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc2157df-27ac-49f5-9e56-230af2ca6f12</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_1.2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
